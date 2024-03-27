932ad6bc8aa9ecaf3b4ec5a35105a5adb86dad0f
Yutaka Kamei <kamei@yykamei.me>
2024-03-27 23:00:11 +0900

    Move TestClient to src

---
+++ src/TestClient.ts
@@ -0,0 +1,53 @@
+import type { APIClient, IssueBody } from "./APIClient";
+import { DateTime } from "./DateTime";
+import { GitHubIssue } from "./GitHubIssue";
+import { GitHubWorkflow } from "./GitHubWorkflow";
+import { GitHubWorkflowRun } from "./GitHubWorkflowRun";
+
+export class TestClient implements APIClient {
+	async getWorkflows(_owner: string, _repo: string): Promise<GitHubWorkflow[]> {
+		return [new GitHubWorkflow(1234, "My Workflow", "my_workflow.yml")];
+	}
+	async getWorkflow(
+		_owner: string,
+		_repo: string,
+		path: string,
+	): Promise<GitHubWorkflow> {
+		return new GitHubWorkflow(1234, "My Workflow", path);
+	}
+
+	async getWorkflowRuns(
+		_owner: string,
+		_repo: string,
+		workflowId: number,
+	): Promise<GitHubWorkflowRun[]> {
+		return [
+			new GitHubWorkflowRun({
+				id: 123,
+				runNumber: 3,
+				name: null,
+				displayTitle: "abc",
+				path: "abc.yml",
+				event: "push",
+				conclusion: "failure",
+				workflowId,
+				createdAt: new DateTime("2024-02-26T08:59:49Z"),
+				updatedAt: new DateTime("2024-02-26T08:59:49Z"),
+			}),
+		];
+	}
+	async createIssue(
+		_owner: string,
+		_repo: string,
+		_issueBody: IssueBody,
+	): Promise<GitHubIssue> {
+		return new GitHubIssue({
+			id: 234,
+			url: "https://github.com/yykamei/test-repo/issue/234",
+			number: 2340,
+			state: "open",
+			title: "test",
+			body: "body",
+		});
+	}
+}
